78de8fe1f1b73316ff065a48011527a30e126ced
johanpurwadhika <johan_clown@yahoo.com>
2021-01-06 10:57:59 +0700

    buat file installmailserver.sh

---
+++ installmailserver.sh
@@ -0,0 +1,4 @@
+#1 /bin/bash
+
+sudo apt update
+sudo apt install postfix
--- script.sh
+++ script.sh
@@ -1,9 +1,3 @@
 #1 /bin/bash
 
-<<<<<<< HEAD
 echo "hallo dunia"
-ls /etc > list_etc_dir.txt
-sudo apt update
-=======
-echo "hallo dunia"
->>>>>>> parent of 3e993a3 (menambahkan perintah update)
